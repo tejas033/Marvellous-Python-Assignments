5a30764af248d8c2b5b23f12d8fd0811361ded4a
tejas033 <teju.deshmukh03@gmail.com>
2025-05-16 02:12:26 +0530

    Update Assignment3_1.py

--- Assignment_3/Assignment3_1.py
+++ Assignment_3/Assignment3_1.py
@@ -1,1 +1,12 @@
+# take "n" numbers, store them in list, return sum of all numbers
 
+size = int(input("Enter number of elements: "))
+lstElements = list()
+
+sum = 0
+print("Input Elements: ")
+for i in range (size):
+    lstElements.append(int(input()))
+    sum = sum + lstElements[i]
+
+print("Output: ", sum)
