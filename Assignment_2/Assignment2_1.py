a3c044010566a49a6de3e9e935488d30eaa84a8b
tejas033 <teju.deshmukh03@gmail.com>
2025-05-16 01:55:58 +0530

    Update Assignment2_1.py

--- Assignment_2/Assignment2_1.py
+++ Assignment_2/Assignment2_1.py
@@ -1,1 +1,17 @@
+# create module > 4 arithmetic methods
+# use this module in your python program that takes 2 numbers and pass them to above module methods
 
+from Arithmetic import Add, Sub, Mult, Div
+
+def main():
+
+    no1 = int(input("Input no1: "))
+    no2 = int(input("Input no2: "))
+
+    print("Addition is: ",Add(no1, no2))
+    print("Subtraction is: ",Sub(no1, no2))
+    print("Multiplication is: ",Mult(no1, no2))
+    print("Division is: ",Div(no1, no2))
+
+if __name__ == "__main__":
+    main()
