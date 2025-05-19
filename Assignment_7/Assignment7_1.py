2371bca7d2d0f8d9c6b91680f7d21283f5669527
tejas033 <teju.deshmukh03@gmail.com>
2025-05-20 00:10:19 +0530

    Update Assignment7_1.py

--- Assignment_7/Assignment7_1.py
+++ Assignment_7/Assignment7_1.py
@@ -1,1 +1,13 @@
+# Assignment7_1.py
+# lambda function to calculate square and cube of a number
 
+Square = lambda no1 : no1 * no1
+Cube = lambda no1 : no1 ** 3
+
+def main():
+    number = int(input("Enter a number: "))
+    print("Square:", Square(number))
+    print("Cube:", Cube(number))
+
+if __name__ == "__main__":
+    main()
