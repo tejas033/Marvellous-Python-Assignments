e83a84a234425f2422e95417be63e68c6ac06377
tejas033 <teju.deshmukh03@gmail.com>
2025-05-19 19:11:47 +0530

    Update Assignment6_1.py

--- Assignment_6/Assignment6_1.py
+++ Assignment_6/Assignment6_1.py
@@ -1,1 +1,16 @@
+# Assignment6_1.py
 
+# While loop demo, print 1 to 50
+
+i = 1
+while(i <= 50):
+    print(i, end=" ")
+    i = i + 1
+
+# i = 1
+# while(i <= 50):
+#     if(i == 5 and i != 50):
+#         print("...", end=" ")
+#     elif((i >= 1 and i <= 4) or i == 50):
+#         print(i, end=" ")
+#     i = i + 1
